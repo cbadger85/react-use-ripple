76a351ed820fe414a2aaede5c392f59341641ef7
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2020-11-27 23:20:15 -0600

    add option to exclude refs from ripple

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useRipple } from 'react-use-ripple';
-import { useRef } from 'react';
 
 const App = () => {
   const ref = useRef();
